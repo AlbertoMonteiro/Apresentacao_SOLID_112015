2bef4ff0b0a8dfd6d9319547cb08c120d1571da2
Alberto Monteiro <alberto.monteiro@live.com>
2015-12-04 14:00:43 -0300

    Update ICalculadorRegra.cs

--- OCP/C#/aplicado/ICalculadorRegra.cs
+++ OCP/C#/aplicado/ICalculadorRegra.cs
@@ -1,36 +1,31 @@
 interface ICalculadorRegra
 {
-	bool PodeCalcular(string medida);
-	decimal Calcular(ItemCarrinho item);
+    bool PodeCalcular(string medida);
+    decimal Calcular(ItemCarrinho item);
 }
 
 class CalculadorRegraUnidade : ICalculadorRegra
 {
-	bool PodeCalcular(string medida)
-		=> medida == "unidade";
-		
-	decimal Calcular(ItemCarrinho item)
-		=> item.Quantidade * 5;
+    bool PodeCalcular(string medida) => medida == "unidade";
+
+    decimal Calcular(ItemCarrinho item) => item.Quantidade * 5;
 }
 
 class CalculadorRegraPeso : ICalculadorRegra
 {
-	bool PodeCalcular(string medida)
-		=> medida == "peso";
-		
-	decimal Calcular(ItemCarrinho item)
-		=> item.Quantidade * 4 / 1000;
+    bool PodeCalcular(string medida) => medida == "peso";
+
+    decimal Calcular(ItemCarrinho item) => item.Quantidade * 4 / 1000;
 }
 
 class CalculadorRegraEspecial : ICalculadorRegra
 {
-	bool PodeCalcular(string medida)
-		=> medida == "especial";
-		
-	decimal Calcular(ItemCarrinho item)
-	{
-		var total = item.Quantidade * 0.4;
-  		var pacoteDeTres = item.Quantidade / 3;
-  		return total - pacoteDeTres * 0.2;
-	}
+    bool PodeCalcular(string medida) => medida == "especial";
+
+    decimal Calcular(ItemCarrinho item)
+    {
+        var total = item.Quantidade * 0.4;
+        var pacoteDeTres = item.Quantidade / 3;
+        return total - pacoteDeTres * 0.2;
+    }
 }
